29e79221e9b8e6e47350b3d6e29d30d781c340aa
yasminebenmouhamed <165170307+yasminebenmouhamed@users.noreply.github.com>
2024-12-28 23:45:28 +0100

    Create Course.js

---
+++ stage/my-backend/models/Course.js
@@ -0,0 +1,9 @@
+const mongoose = require("mongoose");
+
+const courseSchema = new mongoose.Schema({
+  title: { type: String, required: true },
+  price: { type: String, required: true },
+  image: { type: String, required: true }, // URL or path to the image
+});
+
+module.exports = mongoose.model("Course", courseSchema);
